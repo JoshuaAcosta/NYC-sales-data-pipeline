14a249c551789ad3c4ea13c2ab968660a62aed89
Joshua Acosta <Acosta.joshua03@gmail.com>
2019-05-01 07:46:18 -0400

    cleaning up conflict

--- download_data.py
+++ download_data.py
@@ -19,7 +19,6 @@ def get_sales_links(url):
         base = "https://www1.nyc.gov"
         website_text = requests.get(url).text
         soup = BeautifulSoup(website_text, "html5lib")
-<<<<<<< HEAD
         links = soup.select("a[href*='.xls']")
 
         list_of_urls = [base + each['href'] for each in links]
@@ -27,15 +26,7 @@ def get_sales_links(url):
         del list_of_urls[:17]
         sales_list_11_18 = list_of_urls[0:40]
         sales_list_03_10 = list_of_urls[40:]
-=======
-        links = soup.select("a[href$='.xls']")
 
-        list_of_urls = [base + each['href'] for each in links]
-
-        del list_of_urls[:8]
-        sales_list_11_17 = list_of_urls[0:35]
-        sales_list_03_10 = list_of_urls[35:]
->>>>>>> 36bae1701b2061d3b2b58e106e1e2bad619773f7
 
     except requests.RequestException as exception:
         return exception
@@ -55,20 +46,12 @@ def read_excel_data(list_of_urls, skip_rows_num):
     """
     Reads all .xls url links into pandas, selects specific columns.
     """
-
-<<<<<<< HEAD
     use_col_names = ["BOROUGH", "NEIGHBORHOOD", "BUILDING CLASS CATEGORY",\
                      "ADDRESS", "APARTMENT NUMBER", "ZIP CODE",\
                      "RESIDENTIAL UNITS", "COMMERCIAL UNITS", "TOTAL UNITS",\
                      "LAND SQUARE FEET", "GROSS SQUARE FEET", "YEAR BUILT",\
                      "BUILDING CLASS AT TIME OF SALE",\
                      "SALE PRICE", "SALE DATE"]
-=======
-    for each in list_of_urls:
-        response = requests.get(each)
-        filename = each.rsplit('/', 1)[-1]
-        path = data_directory + filename
->>>>>>> 36bae1701b2061d3b2b58e106e1e2bad619773f7
 
     col_str = "A:C,I:Q, S, T, U"
 
@@ -87,7 +70,6 @@ def concat_dfs():
     archived_2011_2018_df = read_excel_data(sales_2011_2018, 4)
     archived_2003_2010_df = read_excel_data(sales_2003_2010, 3)
 
-<<<<<<< HEAD
     combined_df = pd.concat([archived_2011_2018_df, archived_2003_2010_df], ignore_index=True, sort=False)
 
     check_for_data_dir("data/")
@@ -97,10 +79,4 @@ def concat_dfs():
 if __name__ == "__main__":
 
     concat_dfs()
-=======
-    sales_2011_2017, sales_2003_2010 = get_sales_links(SALES_DATA_URL)
 
-    download_files(sales_2011_2017, "data/archived 2011-2017/")
-
-    download_files(sales_2003_2010, "data/archived 2003-2010/")
->>>>>>> 36bae1701b2061d3b2b58e106e1e2bad619773f7
